628bc2b97444bf1287ae4f2512f3efa17d5e31f0
EliasK93 <elias.kempkens@hhu.de>
2019-12-06 00:09:24 +0100

    Added a form to add companies to the database

--- src/main/java/hibernateexample/web/HibernateExampleApplicationController.java
+++ src/main/java/hibernateexample/web/HibernateExampleApplicationController.java
@@ -4,6 +4,10 @@ import hibernateexample.database.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.LinkedList;
 
 @Controller
@@ -26,6 +30,22 @@ public class HibernateExampleApplicationController {
     public String details(Model model, Long company) {
         model.addAttribute("company", repository.findCompanyById(company));
         return "profile";
+    }
+
+    @GetMapping("/new_company")
+    public String addCompanyForm() {
+        return "new_company";
+    }
+
+    @RequestMapping(path = "/company_added", method = RequestMethod.POST)
+    public String showAddCompanySuccess(
+            @RequestParam(name = "name", required = true) String name,
+            @RequestParam(name = "about", required = true) String about,
+            Model model) {
+        Company company = new Company(name, about);
+        repository.save(company);
+        model.addAttribute("company", company);
+        return "company_added";
     }
 
     private void fillWithExamplesIfEmpty() {
